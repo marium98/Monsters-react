4565cc4b202fdf62a6bf28cdde3e69860ad80323
marium98 <mariumadnan98@gmail.com>
2020-04-30 01:53:30 +0500

    Updates

---
+++ precache-manifest.634754057e44875491d330a0e7e07a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0674833fe671133c2a7ef0ecba55a8",
+    "url": "/Monsters-react/index.html"
+  },
+  {
+    "revision": "5c3c95e595ed56cb4ec7",
+    "url": "/Monsters-react/static/css/main.d21abea8.chunk.css"
+  },
+  {
+    "revision": "c1b73a997e4813e9274b",
+    "url": "/Monsters-react/static/js/2.110bae5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-react/static/js/2.110bae5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3c95e595ed56cb4ec7",
+    "url": "/Monsters-react/static/js/main.c0a3d2a0.chunk.js"
+  },
+  {
+    "revision": "dc29bed41b27584daf6b",
+    "url": "/Monsters-react/static/js/runtime-main.35d1788f.js"
+  }
+]);
